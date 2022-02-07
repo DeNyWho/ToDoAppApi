2c4e88bc7da858151bc1fb8fef7b779b665489ae
Ахунов Денис <denis.akhunov00@mail.ru>
2022-02-07 15:37:07 +0500

    Start new project: TodoApp with ktor API

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val exposedVersion: String by project
+val postgresVersion: String by project
+val hikaricpVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.zaxxer:HikariCP:$hikaricpVersion")
+
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-sessions:$ktorVersion")
+    implementation("io.ktor:ktor-locations:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+}
---
+++ src/main/kotlin/com/example/data/User.kt
@@ -0,0 +1,8 @@
+package com.example.data
+
+data class User(
+    val userId: Int,
+    val email: String,
+    val name: String,
+    val password: String,
+)
